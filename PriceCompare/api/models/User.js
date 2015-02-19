013e88e0a2caa3c899a345d29b9331eaa690fbab
DEVDESK <swoolsey@devdesk.net>
2015-02-19 13:26:00 -0800

    defined user attributes and set up bcrypt

--- PriceCompare/api/models/User.js
+++ PriceCompare/api/models/User.js
@@ -8,7 +8,35 @@
 module.exports = {
 
   attributes: {
+    firstName:{
+      type:'string'
+    },
+    lastName:{
+      type:'string'
+    },
+    email:{
+      type:'email',
+      required:true,
+      unique:true
+    },
+    password:{
+      type:'string',
+      required:true
+    },
 
+    toJSON: function(){
+      var userObj = this.toObject();
+      delete userObj.password;
+      return userObj;
+    }
+  },
+
+  beforeCreate:function(values,cb){
+    bcrypt.hash(values.password,10,function(err,hash){
+      if(err) return cb(err);
+      values.password=hash;
+      cb();
+    })
   }
 };
 
