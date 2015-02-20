bf78a1d9ad2eced38c72d2ecef57ec69c7befbfa
DEVDESK <swoolsey@devdesk.net>
2015-02-20 00:02:09 -0800

    begin setting up controller to handle signup and login

--- PriceCompare/assets/js/controllers/AuthModalCtrl.js
+++ PriceCompare/assets/js/controllers/AuthModalCtrl.js
@@ -1,4 +1,3 @@
 PriceCompareApp.controller('AuthModalCtrl',['$scope','$http','$modalInstance','UserService','AlertService',function($scope,$http,$modalInstance,UserService,AlertService){
-  $scope.email="s@s.com";
-  $scope.password="123"
+
 }])
