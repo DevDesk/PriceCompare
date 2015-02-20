58b08310ab6af52c1da064397313d47789daf078
DEVDESK <swoolsey@devdesk.net>
2015-02-19 20:05:05 -0800

    User service post route test

--- PriceCompare/assets/js/services/UserService.js
+++ PriceCompare/assets/js/services/UserService.js
@@ -2,8 +2,16 @@ PriceCompareApp.factory('UserService',['$http',function($http){
 
   return{
     login: function(email,password,callback){
-      alert('this will log the user in')
+      // alert('this will log the user in')
+      var self=this;
+      var loginData={email:email,password:password};
 
+      $http.post('/api/auth',loginData)
+      .success(function(data){
+        alert('success success')
+      }).error(function(err){
+        alert('error')
+      })
     }
 
   }
