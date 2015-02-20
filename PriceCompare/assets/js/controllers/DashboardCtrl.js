73b851c2c690c6fe9fe4fb400c88148481680d3b
DEVDESK <swoolsey@devdesk.net>
2015-02-19 23:59:52 -0800

    testing User Service, requires passing of function as parameter to resolve function not defined error in tests

--- PriceCompare/assets/js/controllers/DashboardCtrl.js
+++ PriceCompare/assets/js/controllers/DashboardCtrl.js
@@ -1,5 +1,7 @@
 PriceCompareApp.controller('DashboardCtrl',['$scope','$http','AlertService','UserService',function($scope,$http,AlertService,UserService){
   $scope.name = "Steve";
-  UserService.login()
+  // $scope.email = "s@s.com";
+  // $scope.password = "123";
+  UserService.login("s@s.com","123",function(){});
   // alert('Alert Test Successful!!!!')
 }])
