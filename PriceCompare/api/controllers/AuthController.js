7849b5dfce5662ab28421377ec9d1a7a7a3fec18
DEVDESK <swoolsey@devdesk.net>
2015-02-19 19:20:15 -0800

    AuthController created and login route authenticating through bcrypt

---
+++ PriceCompare/api/controllers/AuthController.js
@@ -0,0 +1,42 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+//before can use bcrypt, need to require it
+var bcrypt=require('bcrypt')
+
+module.exports = {
+	login:function(req,res){
+    // res.send('this is a login route')
+    User.findOne({where:{email:req.body.email}})
+    .exec(function(err,user){
+      // res.send(user);
+      if(err) return res.send(err);
+      if(user){
+        // res.send(user);
+        bcrypt.compare(req.body.password,user.password,function(err,match){
+          // if(err) return res.send(err);
+          if(match){
+            req.session.user=user;
+            res.send({result:true,user:user});
+          }else{
+            res.send({
+              result:false,
+              error:"Incorrect password."
+            });
+          }
+        })
+      }else{
+        res.send({
+          result:false,
+          error:"User not found. Please signup."
+        });
+      }
+    });
+
+  }
+};
+
