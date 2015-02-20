5f81a0d456fc8894040523abcbf4e1c9bc17023d
DEVDESK <swoolsey@devdesk.net>
2015-02-19 23:58:42 -0800

    complete logout and check portions of User Service

--- PriceCompare/assets/js/services/UserService.js
+++ PriceCompare/assets/js/services/UserService.js
@@ -15,12 +15,36 @@ PriceCompareApp.factory('UserService',['$http',function($http){
           self.currentUser=false;
         }
         //either way need to do a callback
+        // console.log(data);
         callback(null,data);
       }).error(function(err){
         // alert('error')
         callback(err);
       })
+    },
+    logout: function(callback){
+      var self=this;
+      $http.delete('api/auth')
+      .success(function(data){
+        self.currentUser=false;
+        callback(null,data);
+      }).error(function(err){
+        callback(err);
+      })
+    },
+    check: function(callback){
+      var self=this;
+      $http.get('api/auth')
+      .success(function (data){
+        if(data && data.user){
+          console.log(self.currentUser=data.user);
+        }else{
+          console.log(self.currentUser=false)
+        }
+        callback(null,data);
+      }).error(function(err){
+        callback(err);
+      })
     }
-
   }
 }])
