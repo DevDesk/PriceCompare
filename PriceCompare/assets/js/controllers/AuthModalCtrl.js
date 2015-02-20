0d66dade33449beaf79a4349969d1d1094ef320c
DEVDESK <swoolsey@devdesk.net>
2015-02-19 20:24:48 -0800

    tested api/auth post route and returns user data if logged in, false if logged out (via postman)

---
+++ PriceCompare/assets/js/controllers/AuthModalCtrl.js
@@ -0,0 +1,4 @@
+PriceCompareApp.controller('AuthModalCtrl',['$scope','$http','$modalInstance','UserService','AlertService',function($scope,$http,$modalInstance,UserService,AlertService){
+  $scope.email="s@s.com";
+  $scope.password="123"
+}])
--- PriceCompare/assets/js/services/UserService.js
+++ PriceCompare/assets/js/services/UserService.js
@@ -3,18 +3,24 @@ PriceCompareApp.factory('UserService',['$http',function($http){
   return{
     login: function(email,password,callback){
       // alert('this will log the user in')
-      var self=this;
+      var self = this;
       var loginData={email:email,password:password};
 
       $http.post('/api/auth',loginData)
       .success(function(data){
-        alert('success success')
+        // alert('success success')
+        if(data && data.user){
+          self.currentUser=data.user;
+        }else{
+          self.currentUser=false;
+        }
+        //either way need to do a callback
+        callback(null,data);
       }).error(function(err){
-        alert('error')
+        // alert('error')
+        callback(err);
       })
     }
 
   }
-
-
 }])
