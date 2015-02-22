7df7ff1760b75ac7a9c9a3120ccac70648e2cdeb
DEVDESK <swoolsey@devdesk.net>
2015-02-22 15:28:22 -0800

    fixed ngTable and added chart.js

--- PriceCompare/assets/js/app.js
+++ PriceCompare/assets/js/app.js
@@ -1,12 +1,13 @@
 //this is our angular app
-var PriceCompareApp = angular.module('PriceCompare',['ui.bootstrap','ngRoute','bgDirectives']);
+var PriceCompareApp = angular.module('PriceCompare',['ui.bootstrap','ngRoute','bgDirectives','ngTable','chart.js']);
 
 PriceCompareApp.config(['$routeProvider','$locationProvider', function(
   $routeProvider, $locationProvider){
   // console.log ('configured')
   $locationProvider.html5Mode(true);
 
-  //define routes
+  //define routes by <ng-view></ng-view> templateURL=contents, controller is it's controller
+
   $routeProvider
   .when('/',{
     templateUrl:'/views/dashboard.html',
