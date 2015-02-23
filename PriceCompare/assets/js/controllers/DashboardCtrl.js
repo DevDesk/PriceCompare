6f2f6e2af661154ba5367c6e785367a879aad2a0
DEVDESK <swoolsey@devdesk.net>
2015-02-22 18:21:33 -0800

    background styling dashboard, trying different responsive splitter

--- PriceCompare/assets/js/controllers/DashboardCtrl.js
+++ PriceCompare/assets/js/controllers/DashboardCtrl.js
@@ -8,21 +8,17 @@ PriceCompareApp.controller('DashboardCtrl',['$scope','$http','$filter','AlertSer
         {name: "Nephi", age: 29},
         {name: "Enos", age: 34}
     ];
-  // $scope.email = "s@s.com";
-  // $scope.password = "123";
-  // UserService.login("s@s.com","123",function(){});
-  // alert('Alert Test Successful!!!!')
 
-    $scope.tableParams = new ngTableParams({
-        page: 1,            // show first page
-        count: 10           // count per page
-    }, {
-        total: $scope.users.length, // length of data
-        getData: function($defer, params) {
-          var orderedData = params.sorting() ? $filter('orderBy')($scope.users, params.orderBy()) : $scope.users;
-            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-        }
-    });
+  $scope.tableParams = new ngTableParams({
+      page: 1,            // show first page
+      count: 10           // count per page
+  }, {
+      total: $scope.users.length, // length of data
+      getData: function($defer, params) {
+        var orderedData = params.sorting() ? $filter('orderBy')($scope.users, params.orderBy()) : $scope.users;
+          $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+      }
+  });
 
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July","aug","sep","oct","nov","dec"];
   $scope.series = ['Series A', 'Series B'];
@@ -35,51 +31,3 @@ PriceCompareApp.controller('DashboardCtrl',['$scope','$http','$filter','AlertSer
   };
 
 }]);
-
-
-
-// PriceCompareApp.controller('ngTableCtrl',['$scope','ngTableParams','$timeout',function($scope,ngTableParams,$timeout){
-
-//     $scope.$loading = false;
-
-//     if (!$scope.params) {
-//         $scope.params = new ngTableParams();
-//     }
-//     $scope.params.settings().$scope = $scope;
-
-//     var delayFilter = (function () {
-//         var timer = 0;
-//         return function (callback, ms) {
-//             $timeout.cancel(timer);
-//             timer = $timeout(callback, ms);
-//         };
-//     })();
-
-//     $scope.$watch('params.$params', function (newParams, oldParams) {
-//         $scope.params.settings().$scope = $scope;
-
-//         if (!angular.equals(newParams.filter, oldParams.filter)) {
-//             delayFilter(function () {
-//                 $scope.params.$params.page = 1;
-//                 $scope.params.reload();
-//             }, $scope.params.settings().filterDelay);
-//         } else {
-//             $scope.params.reload();
-//         }
-//     }, true);
-
-//     $scope.sortBy = function (column, event) {
-//         var parsedSortable = $scope.parse(column.sortable);
-//         if (!parsedSortable) {
-//             return;
-//         }
-//         var defaultSort = $scope.params.settings().defaultSort;
-//         var inverseSort = (defaultSort === 'asc' ? 'desc' : 'asc');
-//         var sorting = $scope.params.sorting() && $scope.params.sorting()[parsedSortable] && ($scope.params.sorting()[parsedSortable] === defaultSort);
-//         var sortingParams = (event.ctrlKey || event.metaKey) ? $scope.params.sorting() : {};
-//         sortingParams[parsedSortable] = (sorting ? inverseSort : defaultSort);
-//         $scope.params.parameters({
-//             sorting: sortingParams
-//         });
-//     };
-// }];
